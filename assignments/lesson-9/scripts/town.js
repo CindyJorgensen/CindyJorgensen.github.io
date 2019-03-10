341e7e45d21502fe4d9038c5b5f8c0b29ee189a7
CindyJorgensen <jor17017@byui.edu>
2019-03-09 21:46:07 -0900

    week 9

--- assignments/lesson-9/scripts/town.js
+++ assignments/lesson-9/scripts/town.js
@@ -1,4 +1,4 @@
-
+console.log('section');
 var section = document.querySelector('section');
 
 var requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
@@ -10,14 +10,17 @@ request.responseType = 'json';
 request.send();
 
 request.onload = function() {
-    var townData = request.response;
-    showtown(townData);
+    var townInfo = request.response;
+    populateTown(townInfo); 
 }
 
-function showtown(jsonObj) {
-    var townInfo = jsonObj['towns'];
+function populateTown(townInfo) {
         
-    for (var i = 0; i < townInfo.length; i++) {
+    for (var i = 0; i < townInfo.towns.length; i++) {
+!
+      if (townInfo.towns[i].name === 'Preston' || i === 'Soda Springs' || i === 'Fish Haven') {
+        continue; }
+
       var myArticle = document.createElement('article');
       var myH2 = document.createElement('h2');
       var myPara1 = document.createElement('p');
@@ -25,27 +28,39 @@ function showtown(jsonObj) {
       var myPara3 = document.createElement('p');
       var myPara4 = document.createElement('p');
       var myList = document.createElement('ul');
+      var MyImg = document.createElement('img');
+
+
   
-      myH2.textContent = townInfo[i].name;
-      myPara1.textContent = townInfo[i].motto;
-      myPara2.textContent = 'Year Founded: ' + townInfo[i].yearFounded;
-      myPara3.textContent = 'Population: ' + townInfo[i].currentPopulation;
-      myPara4.textContect =' Annual Rain Fall: ' + townInfo[i].averageRainfall;
+      myH2.textContent = townInfo.towns[i].name;
+      myPara1.textContent = townInfo.towns[i].motto;
+      myPara2.textContent = 'Year Founded: ' + townInfo.towns[i].yearFounded;
+      myPara3.textContent = 'Population: ' + townInfo.towns[i].currentPopulation;
+      myPara4.textContect =' Annual Rain Fall: ' + townInfo.towns[i].averageRainfall;
           
-      var eventList = townInfo[i].events;
+    var eventList = townInfo.towns[i].events;
       for (var j = 0; j < eventList.length; j++) {
         var listItem = document.createElement('li');
         listItem.textContent = eventList[j];
         myList.appendChild(listItem);
       }
+/*
+      if (townInfo.towns[i].name === 'Preston') {
+        img.setAttribute("src", "../images/w(3457072585_53e5a224c5_z.jpg"); }
+        else if (townInfo.towns[i].name === 'Soda Springs') {
+          img.setAttribute("src", "../images/w9_6888602099_33b415dfa7_z.jpg");  }
+        else if (townInfo.towns[i].name === 'Fish Haven') {
+          img.setAttribute("src", "../images/w9_15476663331_df4981e838_z.jpg"); } */
   
       myArticle.appendChild(myH2);
       myArticle.appendChild(myPara1);
       myArticle.appendChild(myPara2);
       myArticle.appendChild(myPara3);
-      myArticle.appendChild(myPara4);
+      myArticle.appendChild(myPara4); 
+
       myArticle.appendChild(myList);
-  
+/*      myArticle.appendChild(MyImg); */
+
       section.appendChild(myArticle);
     }
 }
