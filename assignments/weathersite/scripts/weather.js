dad8a9ff7c520f672bb327779bd8dc4636e94c49
CINDY JORGENSEN <cindyjorgensen@CINDYs-Mac-mini.local>
2019-03-23 01:32:02 -0800

    week 11 - update 10

--- assignments/weathersite/scripts/weather.js
+++ assignments/weathersite/scripts/weather.js
@@ -10,11 +10,10 @@ var weatherRequest = new XMLHttpRequest();
 weatherRequest.open('Get', currentURLweather, true);
 
 weatherRequest.responseType = 'json';
-weatherRequest.send();
+weatherRequest.send();      
 
 weatherRequest.onload = function() {
     var weatherData = weatherRequest.response;
-
 
     /* ---- Key: Values ----
         base: "stations"
@@ -43,7 +42,7 @@ weatherRequest.onload = function() {
      + 0.4275 * (weatherData.main.temp) * Math.pow((weatherData.wind.speed),.16)
 
     document.getElementById('windChill').innerHTML = wChill.toFixed(0) + '&deg; F';
-    get5DayForecast(cityid);
+    get5DayForecast(cityid); /* Calls the next function - get5DayForecast(cityid) -->  */
     }
 }
 /* ---------------------------------------------------------------------------------------- *
@@ -52,7 +51,6 @@ weatherRequest.onload = function() {
  *                                                                                          *
  * -----------------------------------------------------------------------------------------*/
 function get5DayForecast(cityid) {
-
 
 var forecastURLweather = 'https://api.openweathermap.org/data/2.5/forecast?id=' + cityid + '&appid=c7d023f4cea5310b318c2e583321df8a&units=imperial'; 
 
@@ -64,7 +62,7 @@ forecastRequest.send();
 
 forecastRequest.onload = function() {
     var forecastData = forecastRequest.response;
-    populateForecast(forecastData);  }
+    populateForecast(forecastData);  } /* Calls the function that will write all content into the city pages */
 }
 
 function populateForecast(forecastData) {
@@ -95,16 +93,13 @@ function populateForecast(forecastData) {
             var myHigh = document.createElement('p');
             var myLow = document.createElement('p');
 
-            var i; 
-            if (a === 0) {
-                i=0;
-                var low = i + 7;}
-                else if (a * 8 <= forecastData.list.length) {
-                    i = (a * 8); /* This finds the list array for the first array of each day  - 0,7,14,21,28,35*/
-                    var low = i + 7; }            
-                else {
-                    break;
-                }
+
+            /**************************************************
+             *   Gets the day of week for the 5 day forecast  *
+             **************************************************/
+
+            var i = a;  /* This was not necessary EXCEPT, I didn't want to rewrite and change the variable therefore assigned that the varibable a = i; because I could have potential missed a variable.*/
+
             var n = new Date(forecastData.list[i].dt_txt);
 
             var weekday = new Array(7);
@@ -119,12 +114,32 @@ function populateForecast(forecastData) {
             myDOWeek.textContent = weekday[n.getDay()];
 
             /* This will sort and output an image */
-            myIcon.setAttribute("src","http://openweathermap.org/img/w/" + forecastData.list[i].weather[0].icon + ".png"),
+            myIcon.setAttribute("src","https://openweathermap.org/img/w/" + forecastData.list[i].weather[0].icon + ".png"),
             ("alt",(forecastData.list[i].weather[0].description)); 
 
-            myHigh.innerHTML = "High: " + forecastData.list[i].main.temp_max.toFixed(0) + "&deg; F";
-            myLow.innerHTML = "Low: " + forecastData.list[i].main.temp_min.toFixed(0) + "&deg; F";
-                
+
+            var highTempTime = /[0-9 :]21:00:00/        /* Sets the time for 9:00 pm; the highest temperature for the day */
+            var x = i-1;
+            do {    /* will run through the statement until it finds that the condition is met */
+              x++;
+            }
+            while (!highTempTime.test(forecastData.list[x].dt_txt)); /* The condition: must meet the time of 9:00pm */
+            var highTemp = x;  /* This sets the variable for the high temperature, without comparising the others */
+          
+            myHigh.innerHTML = "High: " + forecastData.list[highTemp].main.temp_max.toFixed(0) + "&deg; F"; /* Writes the content */
+
+            var lowTempTime = /[0-9 :]09:00:00/  /* This expression sets the low temperature the same way as the high */
+            var l = i-1;
+            do {
+              l++;
+            }
+            while (!lowTempTime.test(forecastData.list[l].dt_txt));
+             
+            var lowTemp = l;
+            myLow.innerHTML = "Low: " + forecastData.list[lowTemp].main.temp_min.toFixed(0) + "&deg; F";
+            
+            if (x > l) { a = x;}  /* This changes the beginning count to exclude all numbers prior to ensure NO duplication */
+                else {a = l;}
                    
             myArticle.appendChild(myDOWeek);
             myArticle.appendChild(myIcon);
