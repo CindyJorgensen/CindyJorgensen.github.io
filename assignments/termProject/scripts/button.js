39af1c8b402d722d90d62b8d6fe0dcf553a59165
CINDY JORGENSEN <cindyjorgensen@CINDYs-Mac-mini.local>
2019-04-11 08:33:05 -0800

    term - upate 10

---
+++ assignments/termProject/scripts/button.js
@@ -0,0 +1,7 @@
+function openForm() {
+    document.getElementById("rewards").style.display = "block";
+}
+
+function closeForm() {
+    document.getElementById("rewards").style.display = "none";
+}
--- assignments/termProject/scripts/hamburger.js
+++ assignments/termProject/scripts/hamburger.js
@@ -1,5 +1,5 @@
 function myFunction() {
-    var x = document.getElementById("myTopnav");
+    var x = document.getElementById("myTopNav");
     if (x.className === "navbar") {
       x.className += " responsive";
     } else {
